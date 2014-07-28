db35d860480cdc850113104da969aabbfeef6b42
Alek <agrinb@gmail.com>
2014-07-28 15:22:26 -0400

    works at number greater than 2

--- fibonacci/lib/fibonacci.rb
+++ fibonacci/lib/fibonacci.rb
@@ -1,1 +1,22 @@
 # YOUR CODE HERE
+#1 1 2 3 5 8 13
+require 'pry'
+
+
+def fibonacci(n)
+  if n > 2
+    sum = fibonacci(n -1 ) + fibonacci(n - 2)
+  else
+    start_n(n)
+  end
+  sum
+end
+
+
+def start_n(n)
+  if n <= 2
+   return 1
+  end
+end
+
+
