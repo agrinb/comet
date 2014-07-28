8b0fb8ed0837dfdf363ff3d3e9a56589915821d1
Alek <agrinb@gmail.com>
2014-07-28 14:30:17 -0400

    tic tac toe done

--- tic_tac_toe/lib/tic_tac_toe.rb
+++ tic_tac_toe/lib/tic_tac_toe.rb
@@ -1,5 +1,58 @@
+require 'pry'
+require 'pry-debugger'
+
 
 def winner?(board)
-  "SOLUTION GOES HERE"
+  if horizontal(board) || diagonal(board)
+    return true
+  else
+    false
+  end
+
+  binding.pry
+
+  board = board.transpose
+
+  if horizontal(board) || diagonal(board)
+    return true
+  else
+    false
+  end
+
 end
+
+def horizontal(board)
+  board.each do |row|
+    unless not_empty?(row)
+      return false
+    end
+
+    if (row[0] == row[1] && row[0] == row[2])
+      return true
+    else
+      false
+    end
+  binding.pry
+  end
+end
+
+def diagonal(board)
+  if board[0][0] == board [1][1] && board[0][0] == board[2][2]
+    return true
+  elsif
+    board[0][2] == board [1][1] && board[0][2] == board[2][0]
+    return true
+  else
+    false
+  end
+end
+
+def not_empty?(row)
+  row.each do |space|
+    if space == " "
+      return false
+    end
+  end
+end
+
 
--- tic_tac_toe/test/tic_tac_toe_test.rb
+++ tic_tac_toe/test/tic_tac_toe_test.rb
@@ -1,6 +1,27 @@
 require_relative '../lib/tic_tac_toe'
 
 describe 'tic tac toe' do
+  it 'finds diagonal winners' do
+    input1 = [['x', ' ', ' '],
+              [' ', 'x', ' '],
+              [' ', ' ', 'x']]
+
+    input2 = [[' ', ' ', 'o'],
+              [' ', 'o', ' '],
+              ['o', ' ', ' ']]
+
+    expect(winner?(input1)).to eq true
+    expect(winner?(input2)).to eq true
+  end
+
+  it 'is false when no winners exist' do
+    input = [['o', ' ', ' '],
+             [' ', ' ', ' '],
+             ['o', ' ', ' ']]
+
+    expect(winner?(input)).to eq false
+  end
+
   it 'finds horizontal winners' do
     input = [['x', 'x', 'x'],
              [' ', ' ', ' '],
@@ -17,13 +38,6 @@ describe 'tic tac toe' do
     expect(winner?(input)).to eq true
   end
 
-  it 'is false when no winners exist' do
-    input = [['o', ' ', ' '],
-             [' ', ' ', ' '],
-             ['o', ' ', ' ']]
-
-    expect(winner?(input)).to eq false
-  end
 
   it 'finds diagonal winners' do
     input1 = [['x', ' ', ' '],
