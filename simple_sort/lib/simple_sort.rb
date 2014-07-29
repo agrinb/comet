04da942e91d34a755275c2cfc08965bbfbaf9afd
Alek <agrinb@gmail.com>
2014-07-29 13:11:35 -0400

    sorting algorithm works

--- simple_sort/lib/simple_sort.rb
+++ simple_sort/lib/simple_sort.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
+array = "30.02 -88.87 10.58 -99.22 107.33"
+
+@@sorted = []
+
 def sort_list(list)
-  "SOLUTION GOES HERE"
+  array = list.split(" ").map { |n| n.to_f }
+  sort(array)
 end
+
+
+def sort(array)
+    min = array.first
+    array.each do |n|
+      if n < min
+        min = n
+      end
+    end
+    array.delete_at(array.index(min))
+  @@sorted << min
+  if array.length > 0
+    sort(array)
+  else
+    @@sorted.join(' ')
+  end
+end
+
+
