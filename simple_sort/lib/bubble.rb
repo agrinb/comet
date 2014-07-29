3139bc0d821678417685168cc561c582a786b326
Alek <agrinb@gmail.com>
2014-07-29 16:37:43 -0400

    cleaner code for bubble sort

--- simple_sort/lib/bubble.rb
+++ simple_sort/lib/bubble.rb
@@ -1,8 +1,5 @@
 require 'pry'
 
-array = "30.02 -88.87 10.58 -99.22 107.33"
-
-@@sorted = []
 
 def sort_list(list)
   array = list.split(" ").map { |n| n.to_f }
@@ -12,19 +9,16 @@ end
 
 def sort(array)
   array.length.times do
-    i = -1
+    i = 0
     while i < array.length - 2 do
-      if array[i+1] > array[i+2]
-        array[i+1], array[i+2] = array[i+2], array[i+1]
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
       end
       i+=1
-      #binding.pry
     end
-    binding.pry
   end
-  puts array
+  array.join(" ")
 end
 
-sort_list(array)
 
 
