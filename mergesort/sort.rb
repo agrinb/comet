132e14d3ed678a61e7b76116b28ecc745e144641
agrinb <agrinb@gmail.com>
2014-09-01 20:16:38 -0400

    merge sort completed

--- mergesort/sort.rb
+++ mergesort/sort.rb
@@ -1,16 +1,14 @@
 require 'pry'
 arr = [ 19, 987, 3, 5, 6, 100, 8, 7, 12, 14 ]
-arr2 = arr.each_slice(1).to_a
 
-@sorted = []
 
 def mergesort(arr)
+  left = []
+  right = []
   if arr.length <= 1
     return arr
   else
     middle = arr.length / 2
-    left = []
-    right = []
     arr.each_with_index do |e,i|
       if i < middle
         left << e
@@ -19,82 +17,46 @@ def mergesort(arr)
       end
     end
   end
-  #binding.pry
   mergesort(left)
   mergesort(right)
-  merge(left, right)
-  # binding.pry
-  # if left.last <= right.first
-  #   left.append(right)
-  # end
+  merge(arr, left, right)
 end
 
 
-def merge(left,right)
- result = []
-  while left.length > 0 && right.length > 0
-    if left.first <= right.first
-      result << left.first
-      binding.pry
-      left.delete_at(0)
 
+
+def merge(arr, left, right)
+  li = 0
+  ri = 0
+  ai = 0
+  while left.length > li && right.length > ri
+    if left[li] < right[ri]
+      arr[ai] = left[li]
+      ai += 1
+      li += 1
+    else
+      arr[ai] = right[ri]
+      ai += 1
+      ri += 1
     end
   end
+
+  while left.length > li
+    arr[ai] = left[li]
+    ai += 1
+    li += 1
+  end
+
+   while right.length > ri
+    arr[ai] = right[ri]
+    ai += 1
+    ri += 1
+  end
+  arr
 end
 
-mergesort(arr)
 
 
-# left = rest(left)
-# elseappend first(right) to result
-# right = rest(right)
-# if length(left) > 0
-# append rest(left) to result
-# if length(right) > 0
-# append rest(right) to result
-# return result
+print mergesort(arr)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def comp(array)
-#   (0..(array.length-1)).each do |x|
-#     new_arr = []
-#     if x % 2 == 0
-#       if array[x][0] <= array[x+1][0]
-#         new_arr = [array[x][0], array[x+1][0]]
-#       else
-#         new_arr = [array[x+1][0], array[x][0]]
-#       end
-#     end
-#     unless new_arr.empty?
-#       @sorted << new_arr
-#     end
-#   end
-#   binding.pry
-#   @sorted
-# end
-
-# @sorted.each do |ineternal_arr|
-#   new_arr = []
-#   internal_arr.each do |smaller
-
-
-# end
-# comp(arr2)
 
